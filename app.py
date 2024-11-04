fe0aa7d979679b0172e28e587fffe9a254969bee
Vapojsjdj <vapotrader7@gmail.com>
2024-11-04 11:16:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -447,5 +447,5 @@ def download():
     except Exception as e:
         return str(e), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    app.run()
